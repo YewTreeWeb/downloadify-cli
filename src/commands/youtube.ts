2233a122d20e3a037d6415e139d9f419f748cf7a
Mathew Teague <18143654+YewTreeWeb@users.noreply.github.com>
2023-10-26 16:13:35 +0100

    feat: added youtube command

--- src/commands/youtube.ts
+++ src/commands/youtube.ts
@@ -1,30 +1,240 @@
-import {Args, Command, Flags} from '@oclif/core'
+import * as os from 'node:os'
+import * as path from 'node:path'
+import * as fs from 'node:fs'
+import { Args, Command, Flags } from '@oclif/core'
+import * as p from '@clack/prompts'
+import color from 'picocolors'
+import {
+  checkFileExists,
+  checkDirExists,
+  deleteOldCookies,
+  fetchCookie,
+  getPrimaryDomain,
+  newDir,
+  ytdl,
+} from '../utils/helper'
+import * as notifier from 'node-notifier'
 
 export default class Youtube extends Command {
   static description = 'describe the command here'
 
-  static examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
+  static examples = ['<%= config.bin %> <%= command.id %>']
 
   static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
+    playlist: Flags.boolean({
+      char: 'p',
+      description:
+        'Download the playlist, if the URL refers to a video and a playlist',
+      required: false,
+    }),
+    verbose: Flags.boolean({
+      char: 'v',
+      description: 'If you want to include debug information in the output',
+      required: false,
+    }),
+    default: Flags.boolean({
+      char: 'd',
+      description:
+        'Skip the majority of the choices and use predefined settings.',
+      required: false,
+    }),
   }
 
   static args = {
-    file: Args.string({description: 'file to read'}),
+    url: Args.string({
+      description: 'The URl of the videos you want to download',
+      required: true,
+    }),
   }
 
   public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Youtube)
+    const { args, flags } = await this.parse(Youtube)
+    const sp = p.spinner()
+    console.clear()
+    p.intro(`${color.bgMagenta(color.black(' YouTube '))}`)
+    const ytOpts = await p.group(
+      {
+        dir: () =>
+          p.text({
+            message: 'What directory would you like to use for your downloads?',
+            initialValue: 'YouTube',
+            validate: (value) => {
+              const regex = /^[A-Za-z-]+$/
+              if (!value) return 'Please enter a directory'
+              if (!regex.test(value))
+                return 'Directory name may only contain letters and dashes'
+            },
+          }),
+        hasDir: async ({ results }) => {
+          // Check to see if directory exists
+          const dirExists = await checkDirExists(
+            `Movies/${String(results.dir)}`,
+          )
+          let dirCreated = false
+          // If directory doesn't exist create it
+          // Else once directory is created, notify user
+          if (!dirExists) {
+            p.log.step(
+              `${color.bgRed(
+                color.white(
+                  `  Directory ${String(results.dir)} does not exist  `,
+                ),
+              )}`,
+            )
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/mat/Developer/downloadify-cli/src/commands/youtube.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+            sp.start(`Now creating ${String(results.dir)}`)
+            await newDir(String(results.dir))
+            sp.stop()
+
+            dirCreated = !dirCreated
+            p.log.step(
+              `${color.bgGreen(
+                color.black(` Successfully created ${String(results.dir)} `),
+              )}`,
+            )
+          } else {
+            dirCreated = !dirCreated
+            p.log.step(
+              `${color.bgGreen(
+                color.black(` Found directory ${String(results.dir)} `),
+              )}`,
+            )
+          }
+          return dirCreated
+        },
+        subtitles: async () => {
+          if (flags.default) return
+          return p.select({
+            message: 'Would you like to download subtitles?',
+            initialValue: 'true',
+            maxItems: 2,
+            options: [
+              { value: 'true', label: 'Yes' },
+              { value: 'false', label: 'No' },
+            ],
+          })
+        },
+        enFormat: async () => {
+          if (flags.default) return
+          return p.select({
+            message: 'Would you like to force the download to be in English?',
+            initialValue: 'No',
+            maxItems: 2,
+            options: [
+              { value: 'true', label: 'Yes' },
+              { value: 'false', label: 'No' },
+            ],
+          })
+        },
+        more: () => {
+          if (flags.default) return
+          return p.select({
+            message: 'Would you like to add any other params to the download?',
+            initialValue: 'false',
+            maxItems: 2,
+            options: [
+              { value: 'true', label: 'Yes' },
+              { value: 'false', label: 'No' },
+            ],
+          })
+        },
+        moreOpts: ({ results }) => {
+          if (results.more === 'false' || flags.default) return
+          return p.text({
+            message: 'What other params would you like to add?',
+          })
+        },
+        confirm: ({ results }) =>
+          p.confirm({
+            message: results.hasDir
+              ? `Download videos to ${results.dir}?`
+              : 'Confirm settings?',
+            initialValue: true,
+          }),
+      },
+      {
+        onCancel: () => {
+          p.cancel(color.bgWhite(color.black('  Download cancelled  ')))
+          process.exit(0)
+        },
+      },
+    )
+
+    // Create an outro for the cli
+    const outro = (msg: string, type: 'abort' | 'error' | 'success') => {
+      const colours: Record<
+        'abort' | 'error' | 'success',
+        (text: string) => string
+      > = {
+        abort: (text) => color.bgBlack(color.white(text)),
+        error: (text) => color.bgRed(color.black(text)),
+        success: (text) => color.bgMagenta(color.black(text)),
+      }
+      const formattedText = colours[type](`  ${msg}  `)
+      return p.outro(formattedText)
+    }
+
+    // If confirm is false
+    if (!ytOpts?.confirm) {
+      outro('Download aborted! Thank you for using Downloadify.', 'abort')
+      process.exit(1)
+    }
+
+    // Ask where to download video
+    const dirName = ytOpts.dir
+    // Set download directory
+    const dwnDir = path.join(os.homedir(), `Movies/${String(dirName)}`)
+
+    // Add arguments to the rest param
+    let rest: string | null = null
+    if (Object.keys(flags).length > 0) {
+      rest = Object.keys(flags)
+        .map((key, i) => {
+          if (key !== 'default') {
+            return key === 'playlist' ? `--yes-${key}` : `--${key}`
+          }
+        })
+        .join(' ')
+    }
+    if (ytOpts.moreOpts && String(ytOpts.moreOpts).length > 0) {
+      const flags = String(ytOpts.moreOpts)
+        .split(' ')
+        .map((flag) => (flag.startsWith('--') ? flag : `--${flag}`))
+        .join(' ')
+      rest += flags
+    }
+
+    let hasFailed: string | boolean = false
+
+    const opts = {
+      location: dwnDir,
+      url: args.url,
+      subs: ytOpts.subtitles === 'true' ? true : false,
+      format: ytOpts.enFormat === 'true',
+      ...(rest && {
+        rest,
+      }),
+    }
+    await ytdl(opts).catch((error) => {
+      if (process.env.NODE_ENV === 'development') console.error(error)
+      hasFailed = error.message
+    })
+
+    if (!hasFailed) {
+      outro(
+        'All downloads completed! Thank you for using Downloadify.',
+        'success',
+      )
+      notifier.notify({
+        title: 'Download Successful',
+        message: `All downloads completed! Thank you for using Downloadify. Completed download`,
+      })
+    } else {
+      outro('An error occurred. Unable to download.', 'error')
+      notifier.notify({
+        title: 'Download Failed',
+        message: `An error occurred. Unable to download - ${hasFailed}`,
+      })
     }
   }
 }
